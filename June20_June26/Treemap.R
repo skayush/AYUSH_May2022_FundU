109b88fcadb0906061f71715c1da5fba5289ba07
Ayush Kumar Singh <singhayushkr2001@gmail.com>
2022-06-24 21:53:16 +0530

    Create Treemap.R

---
+++ June20_June26/Treemap.R
@@ -0,0 +1,42 @@
+library(plotly)
+
+
+labels = c("A1", "A2", "A3", "A4", "A5", "B1", "B2")
+parents = c("", "A1", "A2", "A3", "A4", "", "B1")
+values = c("11", "12", "13", "14", "15", "20", "30")
+
+
+fig1 <- plot_ly(
+  type="treemap",
+  labels=labels,
+  parents=parents,
+  values=values,
+  marker=list(colorscale='Reds'))
+
+fig1
+
+#-------------------------------------------------------------------------------------------
+
+df1 = read.csv('https://raw.githubusercontent.com/plotly/datasets/718417069ead87650b90472464c7565dc8c2cb1c/sunburst-coffee-flavors-complete.csv')
+df2 = read.csv('https://raw.githubusercontent.com/plotly/datasets/718417069ead87650b90472464c7565dc8c2cb1c/coffee-flavors.csv')
+
+
+fig2 <- plot_ly(
+  type='treemap',
+  ids=df1$ids,
+  labels=df1$labels,
+  parents=df1$parents,
+  domain=list(column=0))
+
+
+fig2 <- fig2 %>% add_trace(
+  type='treemap',
+  ids=df2$ids,
+  labels=df2$labels,
+  parents=df2$parents,
+  maxdepth=1,
+  domain=list(column=1))
+
+fig2 <- fig2 %>% layout(grid=list(columns=2, rows=1))
+
+fig2
