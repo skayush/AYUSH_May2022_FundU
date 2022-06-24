8600547c7e9a8b4fef20015eb078888fdc608d53
Ayush Kumar Singh <87649644+skayush@users.noreply.github.com>
2022-06-24 21:50:29 +0530

    Update dygraphs stocks.R

--- June13_June19/dygraphs stocks.R
+++ June13_June19/dygraphs stocks.R
@@ -5,7 +5,7 @@ library(tidyquant)
 library(lubridate)
 library(highcharter)
 
-dataset<- read.csv("June13_June19/Stocks.csv")
+dataset<- read.csv("https://raw.githubusercontent.com/skayush/AYUSH_May2022_FundU/main/June13_June19/Stocks.csv")
 
 
 # We have to convert TIMESTAMP column to date format
